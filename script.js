dba12c7a2af367d9f3cef307efcc2f4787bf19bb
Tachyonite <microbious101@gmail.com>
2023-03-17 12:27:43 +1300

    Updated to add export, pinning (repeatable tasks)

--- script.js
+++ script.js
@@ -3,10 +3,14 @@ const addTaskButton = document.getElementById("addTaskButton");
 const newTaskInput = document.getElementById("newTask");
 const taskList = document.getElementById("taskList");
 const clearDataButton = document.getElementById("clearDataButton");
+const exportDataButton = document.getElementById("exportDataButton");
+const importDataButton = document.getElementById("importDataButton");
+
+
 
 let count = 0;
 
-function createTask(text) {
+function createTask(text, pinned = false) {
   const taskItem = document.createElement("div");
   taskItem.classList.add("task");
 
@@ -26,7 +30,7 @@ function createTask(text) {
   buttonContainer.appendChild(completeButton);  
 
   const deleteButton = document.createElement("button");
-  deleteButton.textContent = "X";
+  deleteButton.textContent = "❌";
   deleteButton.classList.add("delete-button");
   buttonContainer.appendChild(deleteButton);
 
@@ -38,11 +42,38 @@ function createTask(text) {
    completeButton.addEventListener("click", () => {
     taskItem.classList.add("completed");
     setTimeout(() => {
-      taskList.removeChild(taskItem);
       count++;
+      if (!taskItem.classList.contains("pinned")) {
+        taskList.removeChild(taskItem);
+        }
+      else {
+        taskItem.classList.remove("completed");
+      }
       taskCounter.textContent = count;
       saveTasks();
-    }, 1000);
+    }, 200);
+  });
+
+   const pinButton = document.createElement("button");
+  pinButton.textContent = pinned ? "📌" : "📌";
+  pinButton.classList.add("pin-button");
+  buttonContainer.appendChild(pinButton);
+
+  if (pinned) {
+    taskItem.classList.add("pinned");
+  }
+
+  pinButton.addEventListener("click", () => {
+    if (taskItem.classList.contains("pinned")) {
+      taskItem.classList.remove("pinned");
+      pinButton.textContent = "📌";
+      taskList.appendChild(taskItem);
+    } else {
+      taskItem.classList.add("pinned");
+      pinButton.textContent = "📌";
+      taskList.insertBefore(taskItem, taskList.firstChild);
+    }
+    saveTasks();
   });
 
   return taskItem;
@@ -103,7 +134,7 @@ function loadTasks() {
   if (storedTasks) {
     const tasks = JSON.parse(storedTasks);
     tasks.forEach((task) => {
-      const taskItem = createTask(task);
+      const taskItem = createTask(task.text, task.pinned);
       taskList.appendChild(taskItem);
     });
   }
@@ -115,9 +146,12 @@ function loadTasks() {
 }
 
 function saveTasks() {
-  const tasks = Array.from(taskList.children).map((task) => task.querySelector("span").textContent);
+  const tasks = Array.from(taskList.children).map((task) => ({
+    text: task.querySelector("span").textContent,
+    pinned: task.classList.contains("pinned"),
+  }));
   localStorage.setItem("tasks", JSON.stringify(tasks));
-  localStorage.setItem("count", count);
+  localStorage.setItem("count", count.toString());
 }
 
 addTaskButton.addEventListener("click", () => {
@@ -142,3 +176,48 @@ clearDataButton.addEventListener("click", () => {
     clearData();
   }
 });
+
+function exportData() {
+  const tasks = Array.from(taskList.children).map((task) => ({
+    text: task.querySelector("span").textContent,
+    pinned: task.classList.contains("pinned"),
+  }));
+
+  const data = {
+    count,
+    tasks,
+  };
+
+  const dataString = JSON.stringify(data);
+  prompt("Copy the text below to save your data:", dataString);
+}
+
+function importData() {
+  const dataString = prompt("Paste the text you previously copied to import your data:");
+
+  if (dataString) {
+    try {
+      const data = JSON.parse(dataString);
+
+      if (data.count !== undefined && Array.isArray(data.tasks)) {
+        count = data.count;
+        taskCounter.textContent = count;
+
+        taskList.innerHTML = "";
+        data.tasks.forEach((task) => {
+          const taskItem = createTask(task.text, task.pinned);
+          taskList.appendChild(taskItem);
+        });
+
+        saveTasks();
+      } else {
+        alert("Invalid data format. Please ensure you paste the correct data.");
+      }
+    } catch (error) {
+      alert("Error parsing data. Please ensure you paste the correct data.");
+    }
+  }
+}
+
+exportDataButton.addEventListener("click", exportData);
+importDataButton.addEventListener("click", importData);
